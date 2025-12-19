a650fad0b751b874242789d266c5a35b06327824
Micio Dev <micio86dev@gmail.com>
2025-12-19 08:10:49 +0000

    fix: php 8.4 in Docker

--- database/factories/CollectionFactory.php
+++ database/factories/CollectionFactory.php
@@ -12,7 +12,7 @@ class CollectionFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
+            'name' => $this->faker->words(2, true),
             'description' => $this->faker->sentence(),
             'published_at' => $this->faker->optional(0.7)->dateTimeBetween('-5 years', 'now'),
         ];
